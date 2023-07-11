a7662dec938fc96050351fdb2ff64d5efe354a6e
JershDervis <joshdavis16@outlook.com>
2023-07-11 20:41:43 +1000

    Try sveltekit env imports

--- src/lib/database/migrate.ts
+++ src/lib/database/migrate.ts
@@ -2,26 +2,18 @@ import { RDSDataClient } from '@aws-sdk/client-rds-data';
 import { migrate } from 'drizzle-orm/aws-data-api/pg/migrator';
 import { fromEnv } from '@aws-sdk/credential-providers';
 import { drizzle } from 'drizzle-orm/aws-data-api/pg';
-
-import 'dotenv/config';
+import { DB_NAME, AWS_ARN, AWS_ARN_SECRET } from '$env/static/private';
 
 async function runMigrate() {
-	const envVariables = ['DB_NAME', 'AWS_ARN', 'AWS_ARN_SECRET'];
-	envVariables.forEach((element) => {
-		if (!process.env[element]) {
-			throw new Error(`${element} is not defined`);
-		}
-	});
-
 	const rdsClient = new RDSDataClient({
 		credentials: fromEnv(),
 		region: 'ap-southeast-2'
 	});
 
 	const db = drizzle(rdsClient, {
-		database: process.env.DB_NAME as string,
-		resourceArn: process.env.AWS_ARN as string,
-		secretArn: process.env.AWS_ARN_SECRET as string
+		database: DB_NAME,
+		resourceArn: AWS_ARN,
+		secretArn: AWS_ARN_SECRET
 	});
 
 	console.log('Running migrations...');
