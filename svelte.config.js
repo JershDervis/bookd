1ea6bc215ffa07bba93fed3dbd7cb5cb4e97d753
JershDervis <joshdavis16@outlook.com>
2023-07-20 19:30:46 +1000

    Attempting edge deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto'; // auto required for build to work succesfully
-// import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-auto'; // auto required for build to work succesfully
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,8 +15,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			runtime: 'edge'
-			// regions: ['syd1']
+			runtime: 'edge',
+			regions: ['syd1']
 		})
 	}
 };
